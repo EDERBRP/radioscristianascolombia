b7f6ba1eb1f90459d70b7ce273cf0882713ce200
EDERBRP <116520010+EDERBRP@users.noreply.github.com>
2023-04-20 03:06:27 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -255,6 +255,15 @@ function chillHop() {
             audio: "http://radio30.virtualtronics.com:8228/stream",
             active: true,
         },
+        {
+            name:"Hits for Kids",
+            artist: "Kids",
+            cover: "https://png.pngtree.com/png-vector/20190726/ourlarge/pngtree-modern-square-cool-music-for-party-lights-png-image_1629696.jpg",
+            id: uuidv4(),
+            color: ["#008781", "#cfc08c"],
+            audio: "http://s1-webradio.antenne.de/hits-fuer-kids/stream/mp3?aw_0_1st.playerid=tunein.com",
+            active: false,
+        },
     ];
 }
 
